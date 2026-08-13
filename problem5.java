bab77b2421f2a8509f195e027983d154647607bc
sadman-sadek <tasinsadmansadek@gmail.com>
2026-08-13 10:57:20 +0600

    problem 5

---
+++ problem5.java
@@ -0,0 +1,12 @@
+public class Problem5 {
+    public static void main(String[] args) {
+        double width = 5.6;
+        double height = 8.5;
+
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+        System.out.println("Area is = " + area);
+        System.out.println("Perimeter is = " + perimeter);
+    }
+}
